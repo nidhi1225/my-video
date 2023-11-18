ba9db412504ae50fe83438acbce036b901ea2865
Nidhi Rathee <69238973+nidhi1225@users.noreply.github.com>
2023-11-18 13:06:00 +0530

    first

--- src/Composition.tsx
+++ src/Composition.tsx
@@ -1,3 +1,39 @@
-export const MyComposition = () => {
-	return null;
+import React from 'react';
+import { Img, useCurrentFrame, Audio } from 'remotion';
+
+const imageUrls = [
+  'http://localhost:5000/image.jpg',
+  'http://localhost:5000/image2.jpg',
+  'http://localhost:5000/image3.jpg',
+  'http://localhost:5000/image4.jpg',
+  'http://localhost:5000/image5.jpg',
+  'http://localhost:5000/image6.jpg',
+  'http://localhost:5000/image7.jpg',
+  'http://localhost:5000/image8.jpg',
+  // Add more image URLs as needed
+];
+
+const framesPerSecond = 30;
+const totalImages = imageUrls.length;
+const totalDurationInSeconds = 240;
+const durationPerImageInSeconds = totalDurationInSeconds / totalImages;
+
+export const MyComposition: React.FC = () => {
+  const frame = useCurrentFrame();
+
+  const currentImageIndex = Math.floor(frame / (framesPerSecond * durationPerImageInSeconds));
+
+  if (currentImageIndex < 0 || currentImageIndex >= totalImages) {
+    console.warn(`Frame number (${frame}) is outside the valid range.`);
+    return null;
+  }
+
+  const currentImageUrl = imageUrls[currentImageIndex];
+
+  return (
+    <>
+      <Img src={currentImageUrl} width={1920} height={1080} />
+      <Audio src="http://localhost:5000/story.mp3" />
+    </>
+  );
 };
--- src/Root.tsx
+++ src/Root.tsx
@@ -1,17 +1,21 @@
-import {Composition} from 'remotion';
-import {MyComposition} from './Composition';
+import { Composition } from "remotion";
+import { MyComposition } from "./Composition";
 
 export const RemotionRoot: React.FC = () => {
-	return (
-		<>
-			<Composition
-				id="MyComp"
-				component={MyComposition}
-				durationInFrames={60}
-				fps={30}
-				width={1280}
-				height={720}
-			/>
-		</>
-	);
+  const framesPerSecond = 30;
+  const totalDurationInSeconds = 240;
+  const durationInFrames = framesPerSecond * totalDurationInSeconds;
+
+  return (
+    <>
+      <Composition
+        id="MyComposition"
+        durationInFrames={durationInFrames}
+        fps={framesPerSecond}
+        width={1920}
+        height={1080}
+        component={MyComposition}
+      />
+    </>
+  );
 };
